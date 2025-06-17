6e0c671beefdba64e3d0910536f0944b203cc683
jaeyeon <heyblossom@gmail.com>
2025-06-17 15:44:00 +0900

    test

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -9,7 +9,7 @@ const productData = {
   a: {
     name: 'NC-UVA3 Max',
     description: '이건 A3 Max 프린터에 대한 설명입니다.',
-    images: ['/product-a-1.png', '/product-a-2.png', '/product-a-3.png'], // 수정된 부분
+    images: ['/product-a-1.webp', '/product-a-1.webp', '/product-a-1.webp'], // 수정된 부분
     pdf: '/catalog-a.pdf',
   },
   b: {
